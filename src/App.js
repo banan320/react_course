d2e460c28546dc7546c474d8cdc53e6c0bcff7a0
Alex Gerber <alex-gerber@mail.ru>
2023-03-29 17:53:30 +0300

    new lesson

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
+import { React, useState } from "react";
 import "./App.css";
 
-function App() {
-  let buttonText = "Click me";
+function Button({ buttonText, buttonColor, buttonClick }) {
+  return (
+    <>
+      <button className={buttonColor} onClick={buttonClick}>
+        {buttonText}
+      </button>
+    </>
+  );
+}
 
-  const onButton = () => {
-    buttonText = "Hello world!";
-    console.log(buttonText);
-  };
+function App() {
+  const [buttonText, setButtonText] = useState("Click me");
+  const [buttonColor, setButtonColor] = useState("");
+
+  function buttonClick() {
+    setButtonText("Hello world!");
+    setButtonColor("green__btn");
+  }
 
   return (
     <div className="App">
       <div className="app">
-        <button onClick={onButton}>{buttonText}</button>
+        {/* <button className={buttonColor} onClick={buttonClick}>
+          {buttonText}
+        </button> */}
+        <Button
+          buttonColor={buttonColor}
+          buttonText={buttonText}
+          buttonClick={buttonClick}
+        />
       </div>
     </div>
   );
