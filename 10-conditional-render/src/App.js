ae9e4f9d2e7372508822ca8c99cfc0bbe117bd29
Alex Gerber <alex-gerber@mail.ru>
2023-03-30 16:12:16 +0300

    render

--- 10-conditional-render/src/App.js
+++ 10-conditional-render/src/App.js
@@ -1,13 +1,12 @@
 import "./App.css";
 import PetInfo from "./components/PetInfo";
-import OtherComponent from "./components/OtherComponent";
 
 function App() {
   return (
     <div className="App">
-      <PetInfo animal="dog" age={4} />
-      <PetInfo animal="cat" age={5} />
-      <PetInfo animal="mouse" age="1" />
+      <PetInfo animal="dog" age={4} hasPet />
+      <PetInfo animal="cat" age={5} hasPet={true} />
+      <PetInfo animal="mouse" age="1" hasPet={false} />
     </div>
   );
 }
--- 10-conditional-render/src/components/PetInfo.js
+++ 10-conditional-render/src/components/PetInfo.js
@@ -1,14 +1,15 @@
 function PetInfo(props) {
-  const { animal, age } = props;
-  console.log(props);
+  const { animal, age, hasPet } = props;
+
   return (
     <div>
-      <h1>
-        My {props.animal} is {props.age} years old
-      </h1>
-      <h3>
-        My {animal} is {age} years old
-      </h3>
+      {hasPet ? (
+        <h1>
+          My {animal} is {age} years old
+        </h1>
+      ) : (
+        <h2>I dont have an animal</h2>
+      )}
     </div>
   );
 }
