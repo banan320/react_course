1b54ffab50e854462fa2c50f334e6598cf521e4a
Alex Gerber <alex-gerber@mail.ru>
2023-04-03 18:26:21 +0300

    21-jsonplaceholder-posts

---
+++ 21-jsonplaceholder-posts/src/components/Posts.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from "react";
+import Post from "./Post";
+
+const Posts = () => {
+  const [posts, setPosts] = useState([]);
+  const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((json) => setPosts(json))
+      .catch((err) => setError(err.message))
+      .finally(() => setIsLoading(false));
+  }, []);
+
+  if (error) {
+    return <h1 className="error">404 Error: {error}</h1>;
+  }
+
+  return (
+    <>
+      <h1>Posts</h1>
+      <hr />
+      {isLoading ? (
+        <h1>Loading...</h1>
+      ) : (
+        posts.map((post) => <Post key={post.id} {...post} />)
+      )}
+    </>
+  );
+};
+
+export default Posts;
